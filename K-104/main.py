f632e8913d84a9593255b84639733cc13a46d266
Nicat Ceferov <nicat.ceferov14@gmail.com>
2025-06-10 16:23:58 +0400

    Nurlan qovluğunu səliqəyə saldım

---
+++ K-104/main.py
@@ -0,0 +1,32 @@
+# ad = input("Salam, adın nədir? ")
+
+# while ad != 'Aylin':
+#     ad = input('Xeyr, mən başqasını axtarıram. Fərqli ad daxil et ')
+
+# for cehd in range(5):
+#     ad = input(f"{cehd}. Adınızı daxil edin: ")
+
+# print('Erkini tapdım.')
+
+# while True:
+#     ad = input("Adınızı daxil edin: ")
+#     if ad == 'Nicat':
+#         break
+
+# print('Nicatı tapdım')
+
+
+# cocuqlar = []
+
+# say = int(input('Sinifdə necə cocuq var? '))
+
+# for cehd in range(say):
+#     cocuq = input('Cocuğun adını daxil edin: ')
+#     cocuqlar.append(cocuq)
+
+# axtarilan_cocuq = input('Siz hansı cocuqla maraqlınırsınız? ')
+
+# if axtarilan_cocuq in cocuqlar:
+#     print(f'Aa, siz {axtarilan_cocuq}-un valideynisiniz? Sizin cocuq çox yaxşı cocuqdur')
+# else:
+#     print('Hmm. Mən bu cocuğu tanımıram.')
---
+++ K-104/xo.py
@@ -0,0 +1,7 @@
+print('XO oyununa xoş gəlmisiniz')
+oyuncu1 = input('İlk oyunçunun adı: ')
+oyuncu2 = input('İkinci oyunçunun adı: ')
+
+oyun = "- | - | -\n- | - | -\n- | - | -"
+print(oyun)
+oyuncu1_gedisi = int(input(f'{oyuncu1}, zəhmət olmasa 1-9 arasında xana seçin: '))
