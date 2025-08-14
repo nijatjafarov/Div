df63a0525cd575f9ad9e81a7a7e571dc50d572a0
Nicat Ceferov <nicat.ceferov14@gmail.com>
2025-08-14 12:26:50 +0400

    K-117 Rock Paper Scissors finish

--- K-117/Daş Kağız Qayçı/main.js
+++ K-117/Daş Kağız Qayçı/main.js
@@ -1,4 +1,20 @@
 function playGame(userChoice) {
     var choices = ['rock', 'paper', 'scissors'];
-    var choiceOfComputer = choices[];
+    var choiceOfComputer = choices[Math.floor(Math.random()*3)];
+    var coc = document.querySelector('#choice-of-computer');
+    coc.innerHTML = `Choice of computer: ${choiceOfComputer}`;
+    var result;
+
+    if (userChoice == choiceOfComputer) {
+        result = 'Draw';
+    } else if ((userChoice == 'rock' && choiceOfComputer == 'scissors') ||
+               (userChoice == 'paper' && choiceOfComputer == 'rock') ||
+               (userChoice == 'scissors' && choiceOfComputer == 'paper')) {
+        result = 'You win';
+    } else {
+        result = 'Computer wins';
+    }
+
+    var winner = document.querySelector('#winner');
+    winner.innerHTML = result;
 }
